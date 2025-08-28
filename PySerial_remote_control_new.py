1d0b134cd6fab50f0ac0b128803f79382c88cc07
un48ures <felix@letterfix.de>
2025-08-28 09:55:37 +0200

    Replaced threading by more tkinter friendly root.after function; Resending messages when moving sliders; refactored send function; fixed signal strength test mode

--- PySerial_remote_control_new.py
+++ PySerial_remote_control_new.py
@@ -2,29 +2,32 @@ import sys
 import time
 import tkinter as tk
 from tkinter import ttk
-import threading
-
 import serial
 from tkinter import *
 import struct
-
-# Find connected Ports for Arduino
-import serial.tools.list_ports as port_list
-from numpy.matlib import empty
+import serial.tools.list_ports as port_list # Find connected Ports for Arduino
 
 root = tk.Tk()
 root.title("Remote Control LED Poi Sticks")
 
 # Channels: 20 30 40 50 60 70
+# Actually defined by Arduino itself
 receiver_ids = [1, 2, 3, 4, 5, 6]
 
+VIDEO_LIGHT_MODE_HW_BUTTONS = 0
+VIDEO_LIGHT_MODE = 1
+PICTURE_MODE = 2
+SIGNAL_STRENGTH_TEST_MODE = 3
+
 # Protocol:
-# 1.byte - mode
-# 2.byte - receiver_id
-# 3.byte - picture/hue
-# 4.byte - saturation
-# 5.byte - brightness/value
-# 6.byte - velocity
+# 1.byte - START_BYTE - for sync
+# 2.byte - mode
+# 3.byte - receiver id
+# 4.byte - picture
+# 5.byte - hue
+# 6.byte - saturation
+# 7.byte - brightness
+# 8.byte - velocity
 
 # Send bytes
 def send(mode, receiver_id, picture, hue, saturation, brightness_value, velocity):
@@ -71,15 +74,10 @@ def receive():
         except Exception as e:
             print(f"[Binary Receive] Error: {e}")
 
-def send_single_all():
+def send_current_values():
     if receiver_select.get() == -1:  # Select all (-1)
-        send(mode_select.get(), receiver_ids[0], input_picture.get(), hslider.get(), sslider.get(), bslider.get(), vslider.get())
-        send(mode_select.get(), receiver_ids[1], input_picture.get(), hslider.get(), sslider.get(), bslider.get(), vslider.get())
-        send(mode_select.get(), receiver_ids[2], input_picture.get(), hslider.get(), sslider.get(), bslider.get(), vslider.get())
-        send(mode_select.get(), receiver_ids[3], input_picture.get(), hslider.get(), sslider.get(), bslider.get(), vslider.get())
-        send(mode_select.get(), receiver_ids[4], input_picture.get(), hslider.get(), sslider.get(), bslider.get(), vslider.get())
-        send(mode_select.get(), receiver_ids[5], input_picture.get(), hslider.get(), sslider.get(), bslider.get(), vslider.get())
-        # time.sleep(0.25)
+        for identifier in receiver_ids:
+            send(mode_select.get(), identifier, input_picture.get(), hslider.get(), sslider.get(), bslider.get(), vslider.get())
 
     else:
         send(mode_select.get(), receiver_select.get(), input_picture.get(), hslider.get(), sslider.get(), bslider.get(), vslider.get())
@@ -87,12 +85,8 @@ def send_single_all():
 
 def send_off():
     if receiver_select.get() == -1:  # Select all (-1)
-        send(mode_select.get(), receiver_ids[0], 0, 0, 0, 0, 0)
-        send(mode_select.get(), receiver_ids[1], 0, 0, 0, 0, 0)
-        send(mode_select.get(), receiver_ids[2], 0, 0, 0, 0, 0)
-        send(mode_select.get(), receiver_ids[3], 0, 0, 0, 0, 0)
-        send(mode_select.get(), receiver_ids[4], 0, 0, 0, 0, 0)
-        send(mode_select.get(), receiver_ids[5], 0, 0, 0, 0, 0)
+        for identifier in receiver_ids:
+            send(mode_select.get(), identifier, 0, 0, 0, 0, 0)
 
     else:
         send(mode_select.get(), receiver_select.get(), 0, 0, 0, 0, 0)
@@ -102,16 +96,18 @@ def input_inc():
     tmp = input_picture.get()
     tmp = tmp + 1
     input_picture.set(tmp)
-    send_single_all()
-
+    send_current_values()
 
 def input_dec():
     tmp = input_picture.get()
     if tmp > 0:
         tmp = tmp - 1
     input_picture.set(tmp)
-    send_single_all()
+    send_current_values()
 
+# Apply selected COM Port
+def apply():
+    serialPort.setPort(combo.get().split()[0])
 
 
 # Title/Labels
@@ -152,51 +148,6 @@ if len(ports) > 0:
                                stopbits=serial.STOPBITS_ONE)
     combo.set(ports[0])
 
-# Apply selected COM Port
-def apply():
-    serialPort.setPort(combo.get().split()[0])
-
-
-def mode_refresher():
-    mode_select_old = mode_select.get()
-    while 1:
-        if mode_select.get() != mode_select_old:
-            send_single_all()
-        if mode_select.get() == 3:
-            check_for_incoming_receiver_values()
-        if mode_select_old != mode_select.get():
-            mode_select_old = mode_select.get()
-        time.sleep(0.25)
-
-
-def check_for_incoming_receiver_values():
-    if serialPort.in_waiting > 8:
-        print("Receiver Values incoming")
-        # Read data out of the buffer until a carriage return / new line is found
-        serialString = serialPort.readline()
-        res = serialString.decode("Ascii").split()
-        # print("after readLine")
-
-        # Print the contents of the serial data
-        try:
-            inputb1.set(res[0] + "V")
-            inputb2.set(res[1] + "V")
-            inputb3.set(res[2] + "V")
-            inputb4.set(res[3] + "V")
-            inputb5.set(res[4] + "V")
-            inputb6.set(res[5] + "V")
-            inputss1.set(res[6] + "%")
-            inputss2.set(res[7] + "%")
-            inputss3.set(res[8] + "%")
-            inputss4.set(res[9] + "%")
-            inputss5.set(res[10] + "%")
-            inputss6.set(res[11] + "%")
-        except:
-            pass
-
-threading.Thread(target=mode_refresher).start()
-
-
 # Button Apply
 buttonA = tk.Button(root, bg="white", text="Apply", anchor="e", command=apply)
 buttonA.grid(row=8, column=1, sticky="w", padx=10)
@@ -206,7 +157,7 @@ inputlabel = tk.Label(root, bg="white", text="Select Picture Nr.", anchor="n")
 inputlabel.grid(row=1, column=1)
 # Input Box Select Picture Nr.
 input_picture = tk.IntVar()
-input_picture.set('0')
+input_picture.set(0)
 inputbox = tk.Entry(root, textvariable=input_picture, width=5)
 inputbox.grid(row=2, column=1, padx=20)
 # Button Plus
@@ -216,7 +167,7 @@ buttonplus.grid(row=2, column=1, sticky="e")
 buttonminus = tk.Button(root, bg="white", text="-", anchor="e", command=input_dec)
 buttonminus.grid(row=2, column=1, sticky="w", padx=10)
 # Button Send
-button1 = tk.Button(root, text="Send", highlightthickness="6", command=send_single_all)
+button1 = tk.Button(root, text="Send", highlightthickness="6", command=send_current_values)
 button1.grid(row=3, column=1)
 # Button Off
 button2 = tk.Button(root, text="Off", highlightthickness="6", command=send_off)
@@ -238,9 +189,9 @@ receiver_select.set(-1)  # setdefault to CH 20
 bright_label = tk.Label(root, bg="white", text="Brightness/Value", anchor="n")
 bright_label.grid(row=1, column=5)
 # Slider brightness input
-bslider = tk.Scale(root, from_=0, to=8, orient=HORIZONTAL)
+bslider = tk.Scale(root, from_=0, to=40, orient=HORIZONTAL)
 bslider.grid(row=2, column=5)
-bslider.set(1)  # default
+bslider.set(5)  # default
 
 # Slider Hue Label
 hue_label = tk.Label(root, bg="white", text="Hue", anchor="n")
@@ -262,41 +213,41 @@ sslider.set(1)  # default
 vel_label = tk.Label(root, bg="white", text="Velocity", anchor="n")
 vel_label.grid(row=1, column=8)
 # Slider brightness input
-vslider = tk.Scale(root, from_=0, to=255, orient=HORIZONTAL)
+vslider = tk.Scale(root, from_=0, to=60, orient=HORIZONTAL)
 vslider.grid(row=2, column=8)
-vslider.set(1)  # default
+vslider.set(25)  # default
 
 # Battery Level Label
 inputlabel = tk.Label(root, bg="white", text="Battery Level")
 inputlabel.grid(row=1, column=9)
 batteryBoxWidth = 10
 # Input Box Battery 1
-inputb1 = tk.IntVar()
+inputb1 = tk.StringVar()
 inputb1.set('0.0V')
 inputbox = tk.Entry(root, textvariable=inputb1, width=batteryBoxWidth)
 inputbox.grid(row=2, column=9, padx=20)
 # Input Box Battery 2
-inputb2 = tk.IntVar()
+inputb2 = tk.StringVar()
 inputb2.set('0.0V')
 inputbox = tk.Entry(root, textvariable=inputb2, width=batteryBoxWidth)
 inputbox.grid(row=3, column=9, padx=20)
 # Input Box Battery 3
-inputb3 = tk.IntVar()
+inputb3 = tk.StringVar()
 inputb3.set('0.0V')
 inputbox = tk.Entry(root, textvariable=inputb3, width=batteryBoxWidth)
 inputbox.grid(row=4, column=9, padx=20)
 # Input Box Battery 4
-inputb4 = tk.IntVar()
+inputb4 = tk.StringVar()
 inputb4.set('0.0V')
 inputbox = tk.Entry(root, textvariable=inputb4, width=batteryBoxWidth)
 inputbox.grid(row=5, column=9, padx=20)
 # Input Box Battery 5
-inputb5 = tk.IntVar()
+inputb5 = tk.StringVar()
 inputb5.set('0.0V')
 inputbox = tk.Entry(root, textvariable=inputb5, width=batteryBoxWidth)
 inputbox.grid(row=6, column=9, padx=20)
 # Input Box Battery 6
-inputb6 = tk.IntVar()
+inputb6 = tk.StringVar()
 inputb6.set('0.0V')
 inputbox = tk.Entry(root, textvariable=inputb6, width=batteryBoxWidth)
 inputbox.grid(row=7, column=9, padx=20)
@@ -307,41 +258,80 @@ inputlabel = tk.Label(root, bg="white", text="Signal Strength")
 inputlabel.grid(row=1, column=10)
 batteryBoxWidth = 10
 # Input Box Signal Strength 1
-inputss1 = tk.IntVar()
+inputss1 = tk.StringVar()
 inputss1.set('0%')
 inputbox = tk.Entry(root, textvariable=inputss1, width=SignalBoxWidth)
 inputbox.grid(row=2, column=10, padx=20)
 # Input Box Signal Strength 2
-inputss2 = tk.IntVar()
+inputss2 = tk.StringVar()
 inputss2.set('0%')
 inputbox = tk.Entry(root, textvariable=inputss2, width=SignalBoxWidth)
 inputbox.grid(row=3, column=10, padx=20)
 # Input Box Signal Strength 3
-inputss3 = tk.IntVar()
+inputss3 = tk.StringVar()
 inputss3.set('0%')
 inputbox = tk.Entry(root, textvariable=inputss3, width=SignalBoxWidth)
 inputbox.grid(row=4, column=10, padx=20)
 # Input Box Signal Strength 4
-inputss4 = tk.IntVar()
+inputss4 = tk.StringVar()
 inputss4.set('0%')
 inputbox = tk.Entry(root, textvariable=inputss4, width=SignalBoxWidth)
 inputbox.grid(row=5, column=10, padx=20)
 # Input Box Signal Strength 5
-inputss5 = tk.IntVar()
+inputss5 = tk.StringVar()
 inputss5.set('0%')
 inputbox = tk.Entry(root, textvariable=inputss5, width=SignalBoxWidth)
 inputbox.grid(row=6, column=10, padx=20)
 # Input Box Signal Strength 6
-inputss6 = tk.IntVar()
+inputss6 = tk.StringVar()
 inputss6.set('0%')
 inputbox = tk.Entry(root, textvariable=inputss6, width=SignalBoxWidth)
 inputbox.grid(row=7, column=10, padx=20)
 
+mode_select_old = mode_select.get()
+hue_slider_old = hslider.get()
+brightness_slider_old = bslider.get()
+velocity_slider_old = vslider.get()
+
+
+def mode_refresher():
+    global mode_select_old, hue_slider_old, brightness_slider_old, velocity_slider_old
+    #while not stop_event.is_set():
+    current_mode = mode_select.get()
+    if current_mode != mode_select_old:
+        send_current_values()
+
+    if current_mode == SIGNAL_STRENGTH_TEST_MODE:
+        receive()
+
+    if current_mode == VIDEO_LIGHT_MODE:  # Send if hue value slider moved
+        if hslider.get() != hue_slider_old or bslider.get() != brightness_slider_old:
+            hue_slider_old = hslider.get()
+            brightness_slider_old = bslider.get()
+            send_current_values()
+
+    if current_mode == PICTURE_MODE:  # Send if hue value slider moved
+        if vslider.get() != velocity_slider_old or bslider.get() != brightness_slider_old:
+            velocity_slider_old = vslider.get()
+            brightness_slider_old = bslider.get()
+            send_current_values()
+
+    if mode_select_old != current_mode:
+        mode_select_old = current_mode
+    root.after(250, mode_refresher)
+
+
 def exit_program():
+    print("EXit")
+    send_off()
     serialPort.close()
-    root.destroy
+    root.destroy()
 
 # Exit Button
-buttonExit = tk.Button(root, text="Exit", command=root.destroy)
+buttonExit = tk.Button(root, text="Exit", command=exit_program)
 buttonExit.grid(row=9, column=11)
+
+# Main
+root.after(250, mode_refresher) # Call mode refresher every 250ms
 root.mainloop()
+
